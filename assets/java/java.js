5afcb9dbd99c1ab9334c79d3549162d3111bfec3
Kamran <narmak182@gmail.com>
2017-08-12 19:51:39 -0400

    Added Marvel API call for character image, description, and comics the character has appeared in.

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,29 +1,27 @@
 $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function(event) {
 event.preventDefault();
 
+$("#card-content").empty();
+
 var character = $("#character-name").val(); 
+
+var characterid;
+var charactername;
+var charImg;
+var comicImg;
+var p;
+var title;
 
 var publickey = "76cd835d936d952adc72ca7b3647c6e2";
 
 var privatekey = "d988c75463e25f3e311476e6991e26c4a1a36cd5";
 
+var limit = "5";
 
+var format = "comic";
 
 var timestamp = new Date().getTime();
 var hash = CryptoJS.MD5(timestamp + privatekey + publickey).toString();
-
-// var queryURL = "http://gateway.marvel.com:443/v1/public/characters?name=" + character + "&apikey=76cd835d936d952adc72ca7b3647c6e2&hash=" + hash + "&ts=" + timestamp;
-// console.log(queryURL);
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-// }).done(function(response) {
-//  console.log(JSON.stringify(response));
-// });
-
-// Public Key
-// fc0e2b2bcfc273962ce941db43aa2039
 
 var character = $("#character-name").val();
 $(function(){
@@ -32,11 +30,83 @@ $.getJSON( marvelAPI, {
     ts: timestamp,
     apikey: publickey,
     hash: hash,
-    name: character
+    name: character,
+    limit: limit
   })
     .done(function( response ) {
-      console.log(response);
+      console.log(response.data.results);
+      var results = response.data.results;
+
+      for (var i = 0; i < results.length; i++) {
+
+        var imgDiv = $("<div class='item'>");
+        
+        var description = results[i].description;
+        var characterid = results[i].id;
+        var charactername = results[i].name;
+        console.log(charactername);
+
+        console.log(characterid);
+        var imgmod = "/portrait_incredible.jpg"
+
+        var p = $("<p>").text("Description: " + description);
+
+        var charImg = $("<img>");
+        charImg.attr("src", results[i].thumbnail.path + imgmod);
+
+        //imgDiv.prepend(p);
+        //imgDiv.prepend(charImg);
+
+        //$("#test").prepend(imgDiv);
+
+      }
+
+$(function(){
+var marvelAPI2 = 'https://gateway.marvel.com/v1/public/comics';
+$.getJSON( marvelAPI2, {
+    ts: timestamp,
+    apikey: publickey,
+    hash: hash,
+    characters: characterid,
+    limit: limit,
+    format: format
+  })
+    .done(function(response) {
+      console.log(response.data.results);
+      console.log(characterid);
+      var results = response.data.results;
+      console.log(response.data.results[0].urls[0].url);
+
+      for (var i = 0; i < results.length; i++) {
+
+        var title = results[i].title;
+        var imgurl = response.data.results[i].urls[0].url;
+        console.log(imgurl);
+        var comiccover = "/portrait_xlarge.jpg";
+        //var comicimgDiv = $("<div class='item'>");
+
+        var comicImg = $("<img>");
+        comicImg.attr({src: results[i].thumbnail.path + comiccover, url: imgurl, class: "searchimg"});
+
+        //comicimgDiv.prepend(comicImg);
+        //comicimgDiv.prepend(title);
+      
+      }
+
+     $("#card-content").append("<div class='col s12 m7'>" + "<h2 class='header'>" + charactername + "</h2>" + "<div class='card horizontal'>" + "<div class='card-image'>" + charImg + "</div>" + "<div class='card-stacked'>" + "<div class='card-content'>" + p + "</div>" + "<div class='card-action'>" + comicImg + title + "</div>" + "</div>" + "</div>" + "</div>");
+
   });
-   
+
 });
+
+});
+
+});
+
+});
+
+$(document).on("click", ".searchimg", function(){
+console.log(this);
+console.log($(this).attr("url"));
+  window.open($(this).attr("url"));
 })
