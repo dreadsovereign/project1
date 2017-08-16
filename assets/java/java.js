72a8926dbe079b336c2a5357069779c739859860
msgctcheng <msgctcheng@gmail.com>
2017-08-16 15:24:38 -0400

    complete Merge and Animations - Need Firebase

--- assets/java/java.js
+++ assets/java/java.js
@@ -13,6 +13,26 @@ $(document).on("keyup", function (){
  });
 
 var clickCount = 0;
+var character = $("#character-name").val();
+
+var characterid;
+var charactername;
+var charImg;
+var comicImg;
+var p;
+var title;
+var description;
+
+var publickey = "76cd835d936d952adc72ca7b3647c6e2";
+
+var privatekey = "d988c75463e25f3e311476e6991e26c4a1a36cd5";
+
+var limit = "5";
+
+var format = "comic";
+
+var timestamp = new Date().getTime();
+var hash = CryptoJS.MD5(timestamp + privatekey + publickey).toString();
 
 $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function (event) {
     event.preventDefault();
@@ -31,33 +51,9 @@ $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function (event) {
         $(".card").addClass("animated bounceInLeft"); 
         clickCount++
     }
-  
-        
-    
 
-    var character = $("#character-name").val();
-
-    var characterid;
-    var charactername;
-    var charImg;
-    var comicImg;
-    var p;
-    var title;
-    var description;
-
-    var publickey = "76cd835d936d952adc72ca7b3647c6e2";
-
-    var privatekey = "d988c75463e25f3e311476e6991e26c4a1a36cd5";
-
-    var limit = "5";
-
-    var format = "comic";
-
-    var timestamp = new Date().getTime();
-    var hash = CryptoJS.MD5(timestamp + privatekey + publickey).toString();
-
-    var character = $("#character-name").val();
-    $(function () {
+    character = $("#character-name").val();
+    $(function  () {
         var marvelAPI = 'https://gateway.marvel.com/v1/public/characters';
         $.getJSON(marvelAPI, {
             ts: timestamp,
@@ -66,7 +62,7 @@ $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function (event) {
             name: character,
             limit: limit
         })
-            .done(function (response) {
+            .done(function charCall (response) {
                 console.log(response);
                 var results = response.data.results;
 
@@ -76,6 +72,7 @@ $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function (event) {
 
                     description = results[i].description;
                     characterid = results[i].id;
+                   
                     charactername = results[i].name;
                     
                     var imgmod = "/portrait_uncanny.jpg";
@@ -87,51 +84,26 @@ $(".btn.waves-effect.waves-light.blue.darken-4").on("click", function (event) {
                     charImg.attr("src", results[i].thumbnail.path + imgmod);
 
                     var charSpan = $("<span class='card-title'>" + charactername + "</span>");
-                    console.log(charSpan);
+                    console.log(charSpan );
                     setTimeout(function(){
                         $("#marvImg").append(charImg);
                         $("#marvInfo").append(charSpan, description);
                     }, 500);
                 }
+                setTimeout (function(){
+                 comicCall();
+                }, 500);
 
-                $(function () {
-                    var marvelAPI2 = 'https://gateway.marvel.com/v1/public/comics';
-                    $.getJSON(marvelAPI2, {
-                        ts: timestamp,
-                        apikey: publickey,
-                        hash: hash,
-                        characters: characterid,
-                        limit: limit,
-                        format: format
-                    })
-                        .done(function (response) {
-                            console.log(response);
-                            
-                            var results = response.data.results;
-                                
-                            for (var i = 0; i < results.length; i++) {
-                                title = results[i].title;
-                                console.log(title);
-
-                                var imgurl = results[i].urls[0].url;
-                         
-                                var comiccover = "/portrait_medium.jpg";
-
-                                comicImg = $("<img>");
-                                comicImg.attr({ src: results[i].thumbnail.path + comiccover, url: imgurl, class: "searchimg" }).css("margin-right", "5px");
-                                setTimeout(function(){
-                                    $("#marvComs").append(comicImg);
-                                }, 500);
-                            }
-                        });
-                });
             });
     });
     //CHRIS' JS
-    fetchingmovies(character)
+    setTimeout(function(){
+        fetchingmovies(character);
+     
+    }, 600);
+       
     $(".carousel").empty();
     $(".carousel").removeClass("initialized");
-
 
     $.ajax ({
     url: "https://api.walmartlabs.com/v1/search/?query="+character+"&apiKey=zzjd8dnn2xptv4j8nbj8p9mu&format=json",
@@ -161,10 +133,36 @@ $(document).on("click", ".searchimg", function () {
 //Product Clicks
 $(document).on("click", ".product", function () {
     window.open($(this).attr("url"));
-
 })
+function  comicCall() {
+                var marvelAPI2 = 'https://gateway.marvel.com/v1/public/comics';
+                $.getJSON(marvelAPI2, {
+                    ts: timestamp,
+                    apikey: publickey,
+                    hash: hash,
+                    characters: characterid,
+                    limit: limit,
+                    format: format
+                })
+                    .done(function (response) {
+                        
+                        
+                        var results = response.data.results;
+                        for (var i = 0; i < results.length; i++) {
+                            title = results[i].title;
+                  
 
-var secondqueryURL;
+                            var imgurl = results[i].urls[0].url;
+                        
+                            var comiccover = "/portrait_medium.jpg";
+
+                            comicImg = $("<img>");
+                            comicImg.attr({ src: results[i].thumbnail.path + comiccover, url: imgurl, class: "searchimg" }).css("margin-right", "5px");
+                             $("#marvComs").append(comicImg);
+                        }
+                    });
+}
+     
 
 function fetchingmovies(title) {
    
@@ -173,7 +171,7 @@ function fetchingmovies(title) {
     $(".card-action").empty();
     $(".card-title").empty();
     $(".header").empty();
-    
+    var secondqueryURL;
     var j = 0;
     var queryURL = "http://www.omdbapi.com/?s=" + title + "&type = movie&y=&plot=short&apikey=40e9cece";
     var responseReceived;
@@ -187,7 +185,7 @@ function fetchingmovies(title) {
             text = response.Search[i].imdbID
             secondqueryURL = "https://www.omdbapi.com/?i=" + text + "&type = movie&y=&plot=short&apikey=40e9cece";
             $('#mov' + i).append(response.Search[i].Title);
-            $("#img" + i).append('<img class = "movCard card-image" src="' + response.Search[i].Poster + '">');
+            $("#img" + i).append('<img class = "card-image" src="' + response.Search[i].Poster + '">');
             callSecond();
            
         }
@@ -196,13 +194,16 @@ function fetchingmovies(title) {
                 url: secondqueryURL,
                 dataType: 'json'
             }).done(function (data) {
-                $('#plot'+ j).append('<div class= "row card-content">' + data.Plot + '</div>');
+                 console.log(data);
+                 var movPlot = data.Plot;
+                $('#plot'+ j).append("<p>" + movPlot + "</p>");
                 console.log(j);
                 
                 j= j+1;
             });
         
         }
-    });
-    
+    })
 }
+    
+
